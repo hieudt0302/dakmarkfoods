2b5f6d561c55b5e4e37db726641f046e6dac745f
Tôi Là Tùng <toilatung@icloud.com>
2017-11-05 19:37:32 +0700

    fix

--- src/app/Helpers/Blade/FormatPrice.php
+++ src/app/Helpers/Blade/FormatPrice.php
@@ -6,7 +6,7 @@ class FormatPrice {
     public static function price($price, $type = null) {
         switch ($type) {
             case 'vnd':
-                return number_format($price, 2, '.', ','); //etc: 5,500,000.69 
+                return number_format($price, 2, '.', ','); //etc: 5,500,000.69
                 break;
             default:
                 return number_format($price, 2, ',', '.'); //etc: 5.500.000,69
--- src/resources/views/errors/404.blade.php
+++ src/resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title','Trang Không Được Tìm Thấy - Cà Phê Đăk Hà')
 @section('content')
-<div class="container">
+<div class="container pt-50">
     <div class="row">
         <div class="col-xs-12">
             <h1>ERROR: 404 Page not found!</h1>
