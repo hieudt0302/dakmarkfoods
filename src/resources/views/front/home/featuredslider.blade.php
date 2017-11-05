b43ebac7ae945cd6b2b4fe7d98bd29c1699fb84f
hieudt <hieudt0302@gmail.com>
2017-11-05 16:39:38 +0700

    fix slider

--- src/resources/views/front/home/featuredslider.blade.php
+++ src/resources/views/front/home/featuredslider.blade.php
@@ -1,20 +1,17 @@
 <div class="home-section fullwidth-slider bg-dark" id="home">
+    
+    @foreach ($sliders as $slider)
     <!-- Slide Item -->
-    <section class="home-section bg-scroll fixed-height-small bg-dark-alfa-20" data-background="{{ asset('frontend/images/home-sample/01.jpg') }}">
+    <section class="home-section bg-scroll fixed-height-small bg-dark-alfa-20" data-background="{{asset('/storage/'.$slider->image) }}">
         <div class="js-height-parent container">
             <!-- Hero Content -->
             <div class="home-content">
                 <div class="home-text">
-                    <div class="hs-line-8 no-transp mb-40 mb-xs-10">
-                        Hello & Welcome
-                    </div>
                     <div class="hs-line-14 mb-50 mb-xs-10">
-                        We are Rhythm
+                        {{$slider->translation->description}}
                     </div>
                     <div class="local-scroll">
-                        <a href="#" class="btn btn-mod btn-border-w btn-circle btn-small">Get Pricing</a>
-                        <span class="hidden-xs">&nbsp;</span>
-                        <a href="#about" class="btn btn-mod btn-border-w btn-circle btn-small">Learn More</a>
+                        <a href="{{url('/menu')}}/{{$slider->url}}" class="btn btn-mod btn-w btn-round" style="margin-top:-3px;">@lang('common.more-details')</a>
                     </div>
                 </div>
             </div>
@@ -22,48 +19,5 @@
         </div>
     </section>
     <!-- End Slide Item -->
-    <!-- Slide Item -->
-    <section class="home-section bg-scroll fixed-height-small bg-dark-alfa-20" data-background="{{ asset('frontend/images/home-sample/02.jpg') }}">
-        <div class="js-height-parent container">
-            <!-- Hero Content -->
-            <div class="home-content">
-                <div class="home-text">
-                    <h1 class="hs-line-8 no-transp mb-50 mb-xs-10">
-                        We Are just crative people
-                    </h1>
-                    <h2 class="hs-line-14 mb-50 mb-xs-10">
-                        Rhythm Creative Studio
-                    </h2>
-                    <div class="local-scroll">
-                        <a href="#about" class="btn btn-mod btn-border-w btn-small btn-circle hidden-xs">See More</a>
-                        <span class="hidden-xs">&nbsp;</span>
-                        <a href="http://vimeo.com/50201327" class="btn btn-mod btn-border-w btn-small btn-circle lightbox mfp-iframe">Play Reel</a>
-                    </div>
-                </div>
-            </div>
-            <!-- End Hero Content -->
-        </div>
-    </section>
-    <!-- End Slide Item -->
-    <!-- Slide Item -->
-    <section class="home-section bg-scroll fixed-height-small bg-dark-alfa-20" data-background="{{ asset('frontend/images/home-sample/03.jpg') }}">
-        <div class="js-height-parent container">
-            <!-- Hero Content -->
-            <div class="home-content">
-                <div class="home-text">
-                    <h2 class="hs-line-8 no-transp mb-50 mb-xs-10">
-                        Create your dream with
-                    </h2>
-                    <h1 class="hs-line-14 mb-50 mb-xs-10">
-                        Amazing Design
-                    </h1>
-                    <div>
-                        <a href="#" class="btn btn-mod btn-border-w btn-small btn-circle">Get Pricing</a>
-                    </div>
-                </div>
-            </div>
-            <!-- End Hero Content -->
-        </div>
-    </section>
-    <!-- End Slide Item -->
+    @endforeach    
 </div>
