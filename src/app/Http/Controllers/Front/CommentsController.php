e5aaa26eeda09b8bd68b6cdfe6941e49e0031a00
hieudt <hieudt0302@gmail.com>
2017-11-10 22:23:12 +0700

    Update admin source

--- src/app/Http/Controllers/Front/CommentsController.php
+++ src/app/Http/Controllers/Front/CommentsController.php
@@ -42,14 +42,12 @@ class CommentsController extends Controller
             'name' => 'required',
             'email' => 'required|email',
             'comment' => 'required|string',
+            'post_id' =>'required',
         ]);
-
-//        dd($request->all());
 
         if ($validator->fails()) {
             return redirect()->back()
-            ->with('message', 'ERROR-INPUT: Code EI1003')
-            ->with('status', 'danger')
+            ->withErrors($validator)
             ->withInput();
         }
 
@@ -65,7 +63,7 @@ class CommentsController extends Controller
         if(!empty($request->reader_id))
             $comment->author_id = $request->reader_id;
 
-        $post = Post::where('slug', $request->post_slug)->firstOrFail();
+        $post = Post::find($request->post_id); 
 
         $review = $post->comments()->save($comment);
 
