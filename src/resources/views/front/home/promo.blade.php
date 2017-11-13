e5b571d329a4922263342c3ae9f030e007d28b6b
hieudt <hieudt0302@gmail.com>
2017-11-13 16:56:59 +0700

    Updated admin, homepage layout

--- src/resources/views/front/home/promo.blade.php
+++ src/resources/views/front/home/promo.blade.php
@@ -1,28 +1,123 @@
-<div id="adsv2">
-    <div class="row">
-        <div class="col-md-6 col-sm-12 col-xs-12">
-            <div class="ads-left">
-                <a href="/"></a>
-                <div class="ads-text">
-                    <h2>Autumn</h2>
-                    <h1>Sale</h1>
-                    <p>
-                        Sale up to 40% off
-                    </p>                    
+<!-- Section -->
+<section class="page-section ">
+    <div class="container relative">
+
+        <h2 class="section-title font-alt mb-70 mb-sm-40">
+            Bạn đang tìm đặc sản hấp dẫn nhất của từng miền?
+        </h2>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="section-text align-center mb-70 mb-xs-40">
+                    Tố tụng tìm luật sư, tìm kiếm lên Google, cần đặn sản "chất" của 3 miền thì đến với chúng tôi. Không có một cơ hỏi nhỏ nhoi cho lựa chọn sai. Đưa vào miệng là bạn bè bạn cảm nhận dạt dào vùng đất
+                    bạn đã đến.
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-sm-12 col-xs-12">
-            <div class="ads-right">
-                <a href="#"></a>
-                <div class="ads-text ad-r">
-                    <h2 class="ad-r">New products</h2>                    
-                    <h1 class="ad-r">Winged bean tea</h1>
-                    <p class="ad-r">
-                        Coming next week
-                    </p>                    
+
+        <div class="row">
+
+            <!-- Item -->
+            <div class="col-sm-4 mb-20 wow fadeIn" data-wow-delay="0.1s" data-wow-duration="2s">
+
+                <div class="post-prev-img">
+                    <a href="#"><img src="{{ asset('images/foods/mienbac.jpg')}}" alt="" /></a>
                 </div>
+
             </div>
+            <!-- End Item -->
+
+            <!-- Item -->
+            <div class="col-sm-4 mb-20 wow fadeIn" data-wow-delay="0.2s" data-wow-duration="2s">
+
+                <div class="post-prev-img">
+                    <a href="#"><img src="{{ asset('images/foods/mientrung.jpg')}}" alt="" /></a>
+                </div>
+
+            </div>
+            <!-- End Item -->
+
+            <!-- Item -->
+            <div class="col-sm-4 mb-20 wow fadeIn" data-wow-delay="0.3s" data-wow-duration="2s">
+
+                <div class="post-prev-img">
+                    <a href="#"><img src="{{ asset('images/foods/miennam.jpg')}}" alt="" /></a>
+                </div>
+
+            </div>
+            <!-- End Item -->
+
         </div>
+
     </div>
-</div>
+</section>
+<!-- End Section -->
+
+<!-- Divider -->
+<hr class="mt-0 mb-0 " />
+<!-- End Divider -->
+
+
+<!-- Section -->
+<section class="page-section">
+    <div class="container relative">
+
+        <!-- Features Grid -->
+        <div class="row alt-features-grid">
+
+            <!-- Text Item -->
+            <div class="col-sm-3">
+                <div class="alt-features-item align-center">
+                    <div class="alt-features-descr align-left">
+                        <h4 class="mt-0 font-alt">Phương châm của chúng tôi?</h4>
+                        Hoạt động trong lĩnh vực thực phẩm nhiều năm, chúng tôi luôn theo đuổi triêt lý chiều chuộng vị giác khách hàng. Chỉ cần bạn bước 1 bước về phía chúng tôi, 999 bước còn lại chúng tôi sẽ bước.
+                    </div>
+                </div>
+            </div>
+            <!-- End Text Item -->
+
+            <!-- Features Item -->
+            <div class="col-sm-3">
+                <div class="alt-features-item align-center">
+                    <div class="alt-features-icon">
+                        <span class="icon-chat"></span>
+                    </div>
+                    <h3 class="alt-features-title font-alt">Hỗ trợ 24/7</h3>
+                    <p>Luôn luôn lắng nghe.</p>
+                </div>
+            </div>
+            <!-- End Features Item -->
+
+            <!-- Features Item -->
+            <div class="col-sm-3">
+                <div class="alt-features-item align-center">
+                    <div class="alt-features-icon">
+                        <span class="icon-wallet"></span>
+                    </div>
+                    <h3 class="alt-features-title font-alt">100% hoàn tiền</h3>
+                    <p>Nếu bạn thấy chất lượng không như chúng tôi giới thiệu.</p>
+                </div>
+            </div>
+            <!-- End Features Item -->
+
+            <!-- Features Item -->
+            <div class="col-sm-3">
+                <div class="alt-features-item align-center">
+                    <div class="alt-features-icon">
+                        <span class="icon-hotairballoon"></span>
+                    </div>
+                    <h3 class="alt-features-title font-alt">Ship hàng toàn quốc</h3>
+                    <p>Đem cả 3 miền về ngôi nhà bạn.</p>
+                </div>
+            </div>
+            <!-- End Features Item -->
+
+        </div>
+        <!-- End Features Grid -->
+
+    </div>
+</section>
+<!-- End Section -->
+<!-- Divider -->
+<hr class="mt-0 mb-0 " />
+<!-- End Divider -->
