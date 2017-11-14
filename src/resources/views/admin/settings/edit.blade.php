58a28c7a7e2cf28a4f539229c252b3ffaefd066e
hieudt <hieudt0302@gmail.com>
2017-11-14 14:23:28 +0700

    Update admin

--- src/resources/views/admin/settings/edit.blade.php
+++ src/resources/views/admin/settings/edit.blade.php
@@ -198,7 +198,31 @@
 	                                        @endforeach
 	                                    </select>
 	                                </td>
-	                            </tr>         
+	                            </tr>
+	                            <tr>
+	                                <td>Banner style (1:fullwidth, 2:two columns)</td>
+	                                <td>
+	                                    <input type="text" class="form-control" name="banner-style" value="{{ Setting::config('banner-style') }}"/>
+	                                </td>
+	                            </tr>
+	                            <tr>
+	                                <td>Url cho full-width bannder</td>
+	                                <td>
+	                                    <input type="text" class="form-control" name="fw-banner-url" value="{{ Setting::config('fw-banner-url') }}"/>
+	                                </td>
+	                            </tr>
+	                            <tr>
+	                                <td>Url cho bannder bên trái</td>
+	                                <td>
+	                                    <input type="text" class="form-control" name="left-banner-url" value="{{ Setting::config('left-banner-url') }}"/>
+	                                </td>
+	                            </tr> 
+	                            <tr>
+	                                <td>Url cho bannder bên phải</td>
+	                                <td>
+	                                    <input type="text" class="form-control" name="right-banner-url" value="{{ Setting::config('right-banner-url') }}"/>
+	                                </td>
+	                            </tr> 	                             	                              	                                                       	                            	                            		                            	                                     
                             </table>  
                         </div>
                     </div>
